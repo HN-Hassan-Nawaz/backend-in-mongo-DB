dcff9323f1ee37c02f7b685817d7ea10ce44dd21
Hassan Nawaz <hassanpu670@gmail.com>
2024-03-25 01:35:58 +0500

    utills code discussion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import dotenv from "dotenv"
 import connectDB from "./DB/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
+//when DB is connected then it must return a promise so we used .then and .catch
 connectDB()
+    .then(() => {
+        //used app.listen for running on the server 
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is Running at Port ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO DB connection failed !!! ", err)
+    })
 
 
 
+//other way to connect with DB
 /*
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
